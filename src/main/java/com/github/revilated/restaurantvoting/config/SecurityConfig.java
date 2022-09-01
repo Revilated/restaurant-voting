534360033a37adcb01a62d0ceba02283c6194310
Vladimir Nikitin <vv.nikitin@mcc.rsa.ru>
2022-09-01 14:26:12 -0500

    Configure security

---
+++ src/main/java/com/github/revilated/restaurantvoting/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.github.revilated.restaurantvoting.config;
+
+import com.github.revilated.restaurantvoting.model.Role;
+import com.github.revilated.restaurantvoting.model.User;
+import com.github.revilated.restaurantvoting.repository.*;
+import com.github.revilated.restaurantvoting.web.*;
+import lombok.*;
+import lombok.extern.slf4j.*;
+import org.springframework.context.annotation.*;
+import org.springframework.http.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.http.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.crypto.factory.*;
+import org.springframework.security.crypto.password.*;
+import org.springframework.security.web.*;
+
+import java.util.*;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+@AllArgsConstructor
+public class SecurityConfig {
+
+    public static final PasswordEncoder PASSWORD_ENCODER = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PASSWORD_ENCODER;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email -> {
+            log.debug("Authenticating '{}'", email);
+            Optional<User> optionalUser = userRepository.findByEmailIgnoreCase(email);
+            return new AuthUser(optionalUser.orElseThrow(
+                    () -> new UsernameNotFoundException("User '" + email + "' was not found")));
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/admin/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.POST, "/api/profile").anonymous()
+                .antMatchers("/api/**").authenticated()
+                .and().httpBasic()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable();
+        return http.build();
+    }
+}
