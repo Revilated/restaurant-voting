3f8824e72bb20edf9a22bdd563399a857be1c7f8
Vladimir Nikitin <vv.nikitin@mcc.rsa.ru>
2022-09-07 19:23:09 -0500

    Test ProfileMenuController

---
+++ src/test/java/com/github/revilated/restaurantvoting/web/menu/ProfileMenuControllerTest.java
@@ -0,0 +1,34 @@
+package com.github.revilated.restaurantvoting.web.menu;
+
+import com.github.revilated.restaurantvoting.web.*;
+import org.junit.jupiter.api.*;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.*;
+import org.springframework.test.web.servlet.request.*;
+
+import static com.github.revilated.restaurantvoting.web.UserTestData.*;
+import static com.github.revilated.restaurantvoting.web.menu.MenuTestData.*;
+import static com.github.revilated.restaurantvoting.web.restaurant.RestaurantTestData.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class ProfileMenuControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = ProfileMenuController.REST_URL + '/';
+
+    @Test
+    @WithUserDetails(USER1_MAIL)
+    void getAllPerDay() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL, RESTAURANT1_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(DISH_MATCHER.contentJson(restaurant1DailyMenu));
+    }
+
+    @Test
+    void getUnAuth() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL, RESTAURANT1_ID))
+                .andExpect(status().isUnauthorized());
+    }
+}
