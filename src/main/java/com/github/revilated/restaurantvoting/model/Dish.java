6378c2a842f85046392e9c6630e5f5dbbab1ab87
Vladimir Nikitin <vv.nikitin@mcc.rsa.ru>
2022-09-13 10:15:52 -0500

    Increase size of dish.name and optimize index

--- src/main/java/com/github/revilated/restaurantvoting/model/Dish.java
+++ src/main/java/com/github/revilated/restaurantvoting/model/Dish.java
@@ -4,10 +4,12 @@
 package com.github.revilated.restaurantvoting.model;
 
 import com.fasterxml.jackson.annotation.*;
+import com.github.revilated.restaurantvoting.util.validation.*;
 import lombok.*;
 import org.hibernate.validator.constraints.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.*;
 import java.time.*;
 
@@ -19,13 +21,19 @@ import java.time.*;
         name = "dish",
         uniqueConstraints = {@UniqueConstraint(
                 columnNames = {"restaurant_id", "name", "created_date"},
-                name = "dish_unique_restaurant_name_created_idx")
+                name = "dish_unique_restaurant_date_name_idx")
         }
 )
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Setter
-public class Dish extends NamedEntity {
+public class Dish extends BaseEntity {
+
+    @NotBlank
+    @Size(min = 2, max = 255)
+    @Column(name = "name", nullable = false)
+    @NoHtml
+    private String name;
 
     @Column(name = "price", nullable = false)
     @Range(min = 0)
@@ -41,15 +49,22 @@ public class Dish extends NamedEntity {
     private int restaurantId;
 
     public Dish(Integer id, String name, Long price, LocalDate createdDate, int restaurantId) {
-        super(id, name);
+        super(id);
+        this.name = name;
         this.price = price;
         this.createdDate = createdDate;
         this.restaurantId = restaurantId;
     }
 
     public Dish(Integer id, String name, Long price, int restaurantId) {
-        super(id, name);
+        super(id);
+        this.name = name;
         this.price = price;
         this.restaurantId = restaurantId;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + '[' + name + ']';
     }
 }
